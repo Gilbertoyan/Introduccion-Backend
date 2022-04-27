d4af5a8a483bb43038fd5d9a4ffa63b23202d8e7
Gilbertoyan <gilbertoyan7@gmail.com>
2022-04-27 10:35:15 -0600

    Se agrego un index.js y un package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '192.168.50.179';
+
+const port = 3000;
+
+const server = http.createServer((req, res) =>{
+
+    res.statusCode= 200;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Hola mundo\n');
+
+});
+server.listen(port, hostname,() => {
+    console.log('el servidor se está ejecutando en http://$192.168.50.179:3000/');
+});
