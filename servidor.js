4b93d26dad9d507c1502f82873281e86f45ce1c7
Gilbertoyan <gilbertoyan7@gmail.com>
2022-04-28 11:49:17 -0600

    Se crearon los errores

--- servidor.js
+++ servidor.js
@@ -17,7 +17,31 @@ http.createServer( function(request, response){
     '.js' : 'text/javascript',
     '.css' : 'text/css',
     '.png' : 'image/png'
-    
 
-    }
+
+    };
+
+    fs.readFile(filePath, function(error, content){
+        if(error){
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content){
+                    response.writeHead(200, {'Content-Type' : contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else{
+                response.writeHead(500);
+                response.end('sorry, check with the site admin for error: ' +error.
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, {'Content-Type' : contentType});
+            response.end(content, 'utf-8');
+        }
+});            
+    
+}).listen(3000);
+console.log('server running at http://127.0.0.1:3000/');
+    
 }
